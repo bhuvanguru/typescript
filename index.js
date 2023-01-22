53db2330acdf4ea17e3afbf1c2ccd7266846376b
Your Name <you@example.com>
2023-01-22 23:08:34 +0530

    2nd commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function handleClick() {
+    var message = "hello bhuvan";
+    var root = document.createElement('h1');
+    root.textContent = message;
+    document.body.appendChild(root);
+}
---
+++ index.ts
@@ -0,0 +1,6 @@
+function handleClick(){
+    let message:string="hello bhuvan";
+    let root=document.createElement('h1');
+    root.textContent=message;
+    document.body.appendChild(root);
+}
